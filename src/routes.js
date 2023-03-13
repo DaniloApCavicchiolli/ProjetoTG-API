51acab0b7397bf06538a48f773c9de8b61fc1f64
DaniloAp <daniloap.cavicchiolli@gmail.com>
2023-03-12 22:34:01 -0300

    add: Tabela files/ config do multer

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,15 @@
 import { Router } from "express";
+import multer from "multer";
+
 import UserController from "./app/controllers/UserController";
 import AuthController from "./app/controllers/AuthController";
+import FileController from "./app/controllers/FileController";
 
 import authMiddlewares from "./app/middlewares/auth";
+import multerConfig from "./config/multer";
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/auth', AuthController.store);
@@ -12,5 +17,7 @@ routes.post('/auth', AuthController.store);
 routes.use(authMiddlewares); //Qualquer rota que vier abaixo, será uma rota autenticada.
 
 routes.put('/users', UserController.update);
+
+routes.post("/files", upload.single("file"), FileController.store);
 
 export default routes;
